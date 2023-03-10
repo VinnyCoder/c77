9739522b5782880e757c42e60fe578f761663454
VinnyCoder <79372765+VinnyCoder@users.noreply.github.com>
2023-03-10 11:37:22 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var first = vinny;
+var last = singh;
+
+var full = first.concat(last);
+
+console.log(full);
+
+
+var numb1 = 10;
+var numb = 20;
+
+var fullnumb = numb+numb1;
+
+console.log(fullnumb);
